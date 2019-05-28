d72fe983858339064dc088cd358a03a46b4f4a28
陈新 <chenxin01@nationsky.com>
2019-05-28 16:15:03 +0800

    anymodel

---
+++ CXAnyModel/AnyModel/CXAnyModel.h
@@ -0,0 +1,13 @@
+//
+//  CXAnyModel.h
+//  CXAnyModel
+//
+//  Created by shane chen on 2019/5/21.
+//  Copyright © 2019 shane chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CXAnyModel : NSObject <CXAnyModelValues>
+
+@end
---
+++ CXAnyModel/AnyModel/CXAnyModel.m
@@ -0,0 +1,19 @@
+//
+//  CXAnyModel.m
+//  CXAnyModel
+//
+//  Created by shane chen on 2019/5/21.
+//  Copyright © 2019 shane chen. All rights reserved.
+//
+
+#import "CXAnyModel.h"
+#import "CXAnyModelEntrance.h"
+
+@implementation CXAnyModel
+
++ (void)load
+{
+    registerAnyModelToClass(self,nil);
+}
+
+@end
